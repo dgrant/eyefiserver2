6a5cf8b0921c2be88cbbdc7a0eb8b8508c9c90ee
David Grant <davidgrant@gmail.com>
2012-06-24 14:42:13 -0700

    Made the init script use the instance parameter for now to make it work. As a secondary step I will try to make it use the start/stop/restart commands that Vilius added. Just refactoring changes to the .py script

--- usr/local/bin/eyefiserver.py
+++ usr/local/bin/eyefiserver.py
@@ -781,25 +781,31 @@ def runEyeFi():
   #eyeFiLogger.info("Eye-Fi server stopped")
 
 class MyDaemon(Daemon):
-        def run(self):
-                runEyeFi()
+  def run(self):
+    runEyeFi()
+
+def main():
+  pid_file = '/tmp/eyeFiServer.pid'
+  if len(sys.argv) > 2:
+    if 'start' == sys.argv[1]:
+      daemon = MyDaemon(pid_file)
+      daemon.start()
+    elif 'stop' == sys.argv[1]:
+      daemon = MyDaemon(pid_file)
+      daemon.stop()
+    elif 'restart' == sys.argv[1]:
+      daemon = MyDaemon(pid_file)
+      daemon.restart()
+    elif 'instance' == sys.argv[1]:
+      runEyeFi()
+    else:
+      print "Unknown command"
+      sys.exit(2)
+    sys.exit(0)
+  else:
+    print "usage: %s start|stop|restart|instance conf_file log_file" % sys.argv[0]
+    sys.exit(2)
 
 if __name__ == "__main__":
-        daemon = MyDaemon('/tmp/eyeFiServer.pid')
-        if len(sys.argv) > 2:
-                if 'start' == sys.argv[1]:
-                        daemon.start()
-                elif 'stop' == sys.argv[1]:
-                        daemon.stop()
-                elif 'restart' == sys.argv[1]:
-                        daemon.restart()
-                elif 'instance' == sys.argv[1]:
-                        runEyeFi()
-                else:
-                        print "Unknown command"
-                        sys.exit(2)
-                sys.exit(0)
-        else:
-                print "usage: %s start|stop|restart|instance conf_file log_file" % sys.argv[0]
-                sys.exit(2)
+  main()
 
