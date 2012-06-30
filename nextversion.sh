3ba9e2adaa8fddcc7e49cfe665264a6b945e898c
Oleg Burdaev <dukesrg@gmail.com>
2012-06-30 16:28:29 +0400

    Next version test script added

---
+++ nextversion.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+TAG=$(git describe $(git rev-list --all --tags --max-count=1) | awk -F . '{ printf "%d.%d.%d", $1, $2, $3 + 1}')
+git tag -a -m "Deployed $(LANG=en_GB.UTF-8; date)." "$TAG"
+echo "Setting version number to: $TAG"
+sed -i -r "s/^(QPKG_VER=).*/\1\"$TAG\"/" qpkg/qpkg.cfg
+sed -i -r "s/>.*(<\/footer>)/>$TAG\1/" web/index.html
